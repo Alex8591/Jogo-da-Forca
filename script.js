f3d167fece19c084c000bc4af8cfb920d1f00c77
Alex Ribeiro <ribeiromauroalex@gmail.com>
2022-07-06 00:21:33 -0300

    Alteração feita no input 6

--- script.js
+++ script.js
@@ -439,29 +439,7 @@ function adicionarPalavra() {
        
         
     });
-    window.onload = function(e){
-        try{
-            if(window.event) {
-                var charCode = window.event.keyCode;
-               
-            } else if(e) {
-                var charCode = e.which;
-            } else {
-                return true;
-            }
-            if (
-                (charCode > 64 && charCode < 91) ||
-                (charCode > 96 && charCode < 123) ||
-                (charCode > 191 && charCode <= 255)
-            ){
-                return true;
-            } else {
-                return false;
-            }
-        } catch (err) {
-            alert(err.Description);
-        }
-    }
+    
    
     
     botao4.addEventListener("click",salvar);
